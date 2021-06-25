7afa91636e90f1896d4f2519ca221bcae7d24485
smonajemi <sina.monajemi@me.com>
2021-06-24 23:30:24 -0400

    commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -10,33 +10,17 @@ const Movies = ({poster_path, title,overview}) =>
         <Row>
             <Col size='10'>
                 <Card style={{ width: '20rem'}}>
-                <Card.Img variant="top" src={API + poster_path} alt={title}/>
+                    <Card.Img variant="top" src={API + poster_path} alt={title}/>
                     <Card.Body>
                         <Card.Title class='cardTitle' >{title}</Card.Title>
                         {/* <Card.Text>{overview}</Card.Text> */}
                     </Card.Body>
-                <Card.Footer>
-                    <Button variant="primary"><i class="fas fa-thumbs-up"></i></Button>
-                </Card.Footer>
+                    <Card.Footer>
+                        <Button variant="primary"><i class="fas fa-thumbs-up"></i></Button>
+                    </Card.Footer>
                 </Card>
             </Col>
         </Row>
     </Container>
 </div>
-
 export default Movies
-
-
-
-
-// <Card style={{ width: '18rem' }}>
-
-// <Card.Body>
-//     <Card.Title>{title}</Card.Title>
-//     <Card.Text>
-//     Some quick example text to build on the card title and make up the bulk of
-//     the card's content.
-//     </Card.Text>
-//     <Button variant="primary">Go somewhere</Button>
-// </Card.Body>
-// </Card>
