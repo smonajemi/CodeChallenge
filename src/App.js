a4c2ee2aef522f7774abb596a7c849cee0d4990e
smonajemi <sina.monajemi@me.com>
2021-06-24 23:06:37 -0400

    moviesFetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter, Route,Switch} from 'react-router-dom';
 import FetchNav from './components/Nav';
 import FetchMovies from './components/Movies';
 import './App.css';
+const api = `https://api.themoviedb.org/3/movie/popular?api_key=dc4b7ad6e4b986a9636b0bef75dbd914&language=en-US&page=1`;
 
 function App() {
+  const [movies, setData] = useState([]);
+  useEffect(() => {
+    fetch(api)        
+    .then(res => res.json())
+    .then(data =>
+        {
+          console.log(data.results);
+          setData(data.results);
+        });        
+  },[])
   return (
     <div className="App">
       <BrowserRouter>
@@ -14,12 +25,14 @@ function App() {
             <p>HOME PAGE</p>
           </Route>
           <Route exact path='/movies'>
-            <FetchMovies />
+            <div className='moviecontainer'>
+              {movies.length > 0 && movies.map(movie =>(
+                  <FetchMovies key={movie.id} {...movie}/>
+                ))}
+            </div>
           </Route>
         </Switch>
-      </BrowserRouter>
-            
-         
+      </BrowserRouter>        
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,42 +1,41 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, Button} from 'react-bootstrap';
+import {Card, Button, Container,Row,Col} from 'react-bootstrap';
+import '../App.css';
+const API = 'https://image.tmdb.org/t/p/w500';
+const Movies = ({poster_path, title,overview}) =>
+<div className="movie">
+    <Container>
+        <Row>
+            <Col size='10'>
+                <Card style={{ width: '20rem' }}>
+                <Card.Img variant="top" src={API + poster_path} alt={title} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        {/* <Card.Text>{overview}</Card.Text> */}
+                    </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary">Add to Favourits</Button>
+                </Card.Footer>
+                </Card>
+            </Col>
+        </Row>
+    </Container>
+</div>
+
+export default Movies
 
 
-function FetchMovies(){   
-    let movieList = [];
-    const api = `https://api.themoviedb.org/3/movie/popular?api_key=dc4b7ad6e4b986a9636b0bef75dbd914&language=en-US&page=1`;
-    const [data, setData] = useState();
-        fetch(api)        
-        .then(res => res.json())
-        .then(data =>
-            {
-                data.results.map((movie) =>
-                    movieList.push(movie.title)
-                    );
-                
-                console.log(movieList);  
-                setData(movieList);
-                                  
-            })
-        .catch(error => console.error(error));
-    return (
-      <div>
-          <br />
-          <Card style={{width: '18rem'}}>
-            <Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg" />
-            <Card.Body>
-                <Card.Title></Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-        <pre> {JSON.stringify(data,null,2)}</pre>
-      </div>
-    )
-}
 
-export default FetchMovies
+
+// <Card style={{ width: '18rem' }}>
+
+// <Card.Body>
+//     <Card.Title>{title}</Card.Title>
+//     <Card.Text>
+//     Some quick example text to build on the card title and make up the bulk of
+//     the card's content.
+//     </Card.Text>
+//     <Button variant="primary">Go somewhere</Button>
+// </Card.Body>
+// </Card>
