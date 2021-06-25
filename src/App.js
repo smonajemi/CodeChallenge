5f7f9d2a0b33f7b46966c9ad7f5a5e3c4fdc7696
smonajemi <sina.monajemi@me.com>
2021-06-25 08:44:39 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import {BrowserRouter, Route,Switch} from 'react-router-dom';
-import FetchNav from './components/Nav';
-import FetchMovies from './components/Movies';
+import NavBar from './components/Nav';
+import Movies from './components/Movies';
 
 const api = `https://api.themoviedb.org/3/movie/popular?api_key=dc4b7ad6e4b986a9636b0bef75dbd914&language=en-US&page=1`;
 function App() {
   const [movies, setData] = useState([]);
+  const [favoriteMovies, setFavorites] = useState([]);
+
   useEffect(() => {
     fetch(api)        
     .then(res => res.json())
@@ -15,20 +17,31 @@ function App() {
           setData(data.results);
         });        
   },[])
+
+  const addTolist = (movie) => {
+    const newList = [...favoriteMovies,movie];
+    setFavorites(newList);
+  }
   return (
     <div className="App">
       <BrowserRouter>
-        <FetchNav />
+        <NavBar />
         <Switch>
           <Route exact path='/'>
+            <div className='headerText'><h1>Select your favorit movie</h1></div>
           <div className='moviecontainer'>
               {movies.length > 0 && movies.map(movie =>(
-                  <FetchMovies key={movie.id} {...movie}/>
+                  <Movies key={movie.id} {...movie}/>
                 ))}
             </div>
           </Route>
-          <Route exact path='/movies'>
-           <p>LIKED MOVIES</p>
+          <Route exact path='/liked'>
+          <div className='headerText'><h1>Select your favorit movie</h1></div>
+          <div className='moviecontainer'>
+              {favoriteMovies.length > 0 && favoriteMovies.map(movie =>(
+                  <Movies key={movie.id} {...movie} clickOnHandle={addTolist}/>
+                ))}
+            </div>
           </Route>
         </Switch>
       </BrowserRouter>        
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,20 +4,20 @@ import {Card, Container,Row,Col} from 'react-bootstrap';
 import '../lib/font-awesome/css/all.min.css';
 import '../App.css';
 const API = 'https://image.tmdb.org/t/p/w500';
-const Movies = ({poster_path, title,overview,vote_count}) =>
+const Movies = (props) =>
 <div className="movie">
     <Container>
         <Row> 
             <Col>
                 <Card style={{ width: '20rem'}}>
-                    <Card.Img variant="top" src={API + poster_path} alt={title} className='cardImage'/>
+                    <Card.Img variant="top" src={API + props.poster_path} alt={props.title} className='cardImage'/>
                     <Card.Footer className="cardFooter">
                         <Row>
                             <Col>
-                                <i className="fas fa-thumbs-up like"></i>
+                                <i className="fas fa-thumbs-up like" onClick={() => props.clickOnHandle()}></i>
                             </Col>
                             <Col>
-                                <i className="fas fa-eye">  {vote_count}</i>
+                                <i className="fas fa-eye">  {props.vote_count}</i>
                             </Col>
                         </Row>                     
                     </Card.Footer>
